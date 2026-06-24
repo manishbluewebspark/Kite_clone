0fef04866c116ea3b6a8057a9798c544bddac86e
RohitA2 <rohit7067932656@gmail.com>
2026-06-24 16:05:05 +0530

    fix: use VITE_API_URL instead of hardcoded localhost

--- frontend/src/api/api.ts
+++ frontend/src/api/api.ts
@@ -1,10 +1,28 @@
+// import axios from "axios";
+
+// export const api = axios.create({
+//   baseURL: "http://localhost:5000/api",
+//   withCredentials: true,
+// });
+
+
+// api.interceptors.request.use((config) => {
+//   const token = localStorage.getItem("token");
+
+//   if (token && config.headers) {
+//     config.headers.Authorization = `Bearer ${token}`;
+//   }
+
+//   return config;
+// });
+
+
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: import.meta.env.VITE_API_URL || "/api", // ⬅️ env variable use karo
   withCredentials: true,
 });
-
 
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
